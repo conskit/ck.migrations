733d6ebffe5cdcca44df926153c68a6248b13520
Jason Murphy <json.murphy@gmail.com>
2016-07-20 19:48:54 -0500

    Removed dependence on MySQL and specified :resource-paths for dev profile in attempt to fix the issue solved otherwise in the previous commit

--- project.clj
+++ project.clj
@@ -6,12 +6,12 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [conskit "0.2.0-SNAPSHOT"]]
   :profiles {:dev {:source-paths ["dev" "flyway"]
+                   :resource-paths ["dev-resources"]
                    :dependencies [[puppetlabs/trapperkeeper "1.4.1" :classifier "test"]
                                   [puppetlabs/kitchensink "1.3.1" :classifier "test" :scope "test"]
                                   [org.flywaydb/flyway-core "4.0.3"]
                                   [org.clojure/java.jdbc "0.6.2-alpha1"]
                                   [com.h2database/h2 "1.4.192"]
-                                  [mysql/mysql-connector-java "5.1.39"]
                                   [midje "1.8.3"]]
                    :plugins [[lein-midje "3.2"]]}
              :flyway-db {:source-paths ["flyway"]
